5cd889946d89ab16cf49ab109e29a0e790d06144
Matt Coston <mattcoston7@gmail.com>
2021-02-08 18:55:21 -0600

    started CP2

--- cp2/src/App.js
+++ cp2/src/App.js
@@ -1,12 +1,44 @@
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import "./App.css";
+import ButtonAppBar from "./Components/buttonappbar";
+import UsernameField from "./Components/usernamefield";
 
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      TEST
-    </div>
-  );
+class App extends Component {
+  state = {
+    loggedIn: false,
+  };
+  changeState = () => {
+    this.setState({ loggedIn: !this.state.loggedIn });
+    console.log(this.state.loggedIn);
+  };
+
+  render() {
+    if (this.state.loggedIn) {
+      return (
+        <div className="App">
+          <ButtonAppBar />
+
+          <button onClick={this.changeState}>LOG OUT</button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <ButtonAppBar />
+          <UsernameField />
+          <button onClick={this.changeState}>LOGIN</button>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
---
+++ cp2/src/Components/usernamefield.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+
+export default function UsernameField() {
+  return (
+    <div className="">
+      <div>
+        <TextField
+          label="Username"
+          id="margin-dense"
+          className=""
+          helperText="Enter your Username"
+          margin=""
+        />
+        <TextField
+          type="password"
+          label="Password"
+          id="margin-dense"
+          className=""
+          helperText="Enter your Password"
+          margin=""
+        />
+      </div>
+    </div>
+  );
+}
